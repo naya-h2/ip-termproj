2de980da87a8d034d65bf81ab02898399d0590f9
안희원 <hw7666@sogang.ac.kr>
2024-12-26 00:26:14 +0900

    api 끝냈나 ,,?

--- back/index.js
+++ back/index.js
@@ -8,14 +8,16 @@ const Cosmetic = require("./models/cosmetic");
 
 mongoose
   .connect(process.env.MONGO_URI)
-  .then(() => console.log("db 연결 완"))
-  .catch(() => console.log("db 연결 실패"));
+  .then(() => console.log("db 연결이 완료되었습니다."))
+  .catch(() => console.log("db 연결에 실패하였습니다."));
 
 app.use(express.json());
 
-app.get("/search/:keyword", async (req, res) => {
+app.get("/search/:keyword/all", async (req, res) => {
   try {
-    const searchedData = await Cosmetic.find({ name: req.params.keyword });
+    const searchedData = await Cosmetic.find({
+      name: req.params.keyword,
+    });
     res.status(200).json(searchedData);
   } catch (err) {
     res.status(500).json({
@@ -25,6 +27,33 @@ app.get("/search/:keyword", async (req, res) => {
   }
 });
 
+app.get("/search/:keyword", async (req, res) => {
+  try {
+    const startDate = new Date(); //하루 전으로 설정
+    startDate.setDate(startDate.getDate() - 1);
+    startDate.setHours(0, 0, 0, 0);
+
+    const endDate = new Date();
+
+    // console.log(startDate, endDate);
+
+    const searchedData = await Cosmetic.find({
+      name: req.params.keyword,
+      createdAt: {
+        $gte: startDate,
+        $lte: endDate,
+      },
+    });
+    res.status(200).json(searchedData);
+  } catch (err) {
+    res.status(500).json({
+      message: "데이터를 가져오는 데 실패하였습니다.",
+      error: err.message,
+    });
+  }
+});
+
+//전체 데이터 가져오기
 app.get("/cosmetic", async (req, res) => {
   try {
     const cosmetics = await Cosmetic.find({});
@@ -37,12 +66,23 @@ app.get("/cosmetic", async (req, res) => {
   }
 });
 
+//data 생성
 app.post("/cosmetic", async (req, res) => {
   try {
+    const today = new Date();
+    const already = await Cosmetic.find({
+      name: req.body.name,
+      price: req.body.price,
+      createdAt: today,
+    });
+    if (already) throw Error("이미 동일한 데이터가 존재합니다.");
+
     const newCosmetic = new Cosmetic({
       name: req.body.name,
       price: req.body.price,
       place: req.body.place,
+      url: req.body.url,
+      imgUrl: req.body.imgUrl,
     });
     await newCosmetic.save(); //db에 저장
     res.status(201).json({
--- back/models/cosmetic.js
+++ back/models/cosmetic.js
@@ -1,9 +1,26 @@
 const mongoose = require("mongoose");
 
-const cosmeticSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  price: { type: Number, required: true },
-  place: { type: String, required: true },
-});
+const cosmeticSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    price: { type: Number, required: true },
+    place: {
+      type: String,
+      enum: [
+        "공식 홈페이지",
+        "쿠팡",
+        "올리브영",
+        "무신사 뷰티",
+        "지그재그",
+        "에이블리",
+      ],
+      default: "공식 홈페이지",
+      required: true,
+    },
+    url: { type: String, required: true },
+    imgUrl: { type: String, required: false },
+  },
+  { timestamps: true }
+);
 
 module.exports = mongoose.model("Cosmetic", cosmeticSchema);
